7a57b97f44cf85b84b48e19b83bdd95ecd273a0b
jeanpcoelho <jeanpc0018@gmail.com>
2024-02-09 15:39:38 -0300

    Update ClienteRepository.java
    um erro ,ainda nao sei por qual motivo

--- src/main/java/com/apirest/web/Cliente/repository/ClienteRepository.java
+++ src/main/java/com/apirest/web/Cliente/repository/ClienteRepository.java
@@ -1,2 +1,8 @@
 package com.apirest.web.Cliente.repository;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.apirest.web.cliente.entity.Cliente;
+
+public interface ClienteRepository extends JpaRepository<Cliente, Long> {
+}
